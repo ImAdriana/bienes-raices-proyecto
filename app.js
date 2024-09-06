0b5cc6d5cd7b7baa3e3385cb57f68e7f3f72b80d
Im Adriana Clemente <clemente.hernandez.alicia.adriana@gmail.com>
2024-09-05 21:22:00 -0600

    Middleware de año

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 import express from 'express';
+import router from './routes/usuarioRoutes.js';
 
 const app = express();
 
+// Habilitar pug
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Middleware para obtener el año actual
+app.use((req, res, next) => {
+    const anio = new Date();
+    res.locals.actualAnio = anio.getFullYear();
+    res.locals.nombreSitio = 'Agencia viajes | ';
+    return next();
+});
+
+// Definir la carpeta pública
+app.use(express.static('public'));
+
+// Agregar router
+app.use('/', router);
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
